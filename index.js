7564585db64359837f7a4223a4cd28cab64d3b5f
omkar <omkar13021@gmail.com>
2025-02-12 00:07:10 +0530

    Deploy through Elastic Beanstalk

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ import mongoose from "mongoose"
 import cors from "cors"
 import dotenv from "dotenv"
 import helmet from "helmet"
-import morgan from "morgan" 
-import {notFound, errorMiddleware} from "./middleware/error.js"
+import morgan from "morgan"
+import { notFound, errorMiddleware } from "./middleware/error.js"
 
 // import errorMiddleware from "./middleware/error.js"
 import userRoutes from "./routes/user.js"
@@ -17,20 +17,23 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({policy: "cross-origin"}));
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"))
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cors());
 // The order of these routes are important
+app.get('/', (req, res) => {
+    res.send('helo');
+})
 app.use('/users', userRoutes);
-app.use('/professor',professorRoutes);
+app.use('/professor', professorRoutes);
 app.use('/general', generalRoutes);
 app.use(notFound)
 app.use(errorMiddleware)
-const PORT = process.env.PORT  || 5001;
+const PORT = process.env.PORT || 5001;
 mongoose.connect(process.env.MONGO_URL, {
 }).then(() => {
-    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));  
-}).catch((error) => console.log(`${error} did not connect`)); 
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+}).catch((error) => console.log(`${error} did not connect`));
 
